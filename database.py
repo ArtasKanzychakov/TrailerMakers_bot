25de5927dc8bcb28e0c4dc591a38775c85ce066b
ArtasKanzychakov <Artasmovie@yandex.ru>
2025-07-15 15:00:30 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, Column, Integer, String, JSON  
+from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.orm import sessionmaker  
+
+Base = declarative_base()  
+
+class Project(Base):  
+    __tablename__ = 'projects'  
+    id = Column(Integer, primary_key=True)  
+    name = Column(String)  
+    status = Column(String, default="В работе")  
+    brief = Column(String)  
+    client_id = Column(Integer)  
+    editors = Column(JSON)  # Список ID монтажёров  
+    producer_id = Column(Integer)  
+
+engine = create_engine(os.getenv("DATABASE_URL"))  
+Base.metadata.create_all(engine)  
+Session = sessionmaker(bind=engine)  
