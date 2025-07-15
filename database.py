0f3f721bcdab7d4f4bd43852eab5a94c40dbe042
ArtasKanzychakov <Artasmovie@yandex.ru>
2025-07-15 15:31:25 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,23 @@
-from sqlalchemy import create_engine, Column, Integer, String, JSON  
-from sqlalchemy.ext.declarative import declarative_base  
-from sqlalchemy.orm import sessionmaker  
+import os
+from sqlalchemy import create_engine, Column, Integer, String, JSON
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-Base = declarative_base()  
+load_dotenv()
 
-class Project(Base):  
-    __tablename__ = 'projects'  
-    id = Column(Integer, primary_key=True)  
-    name = Column(String)  
-    status = Column(String, default="В работе")  
-    brief = Column(String)  
-    client_id = Column(Integer)  
-    editors = Column(JSON)  # Список ID монтажёров  
-    producer_id = Column(Integer)  
+Base = declarative_base()
 
-engine = create_engine(os.getenv("DATABASE_URL"))  
-Base.metadata.create_all(engine)  
-Session = sessionmaker(bind=engine)  
+class Project(Base):
+    __tablename__ = 'projects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    status = Column(String, default="В работе")
+    brief = Column(String)
+    client_id = Column(Integer)
+    editors = Column(JSON)  # список ID
+    producer_id = Column(Integer)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
