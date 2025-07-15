93f533ef485fbf23f530eaa534c7438a0c061675
ArtasKanzychakov <Artasmovie@yandex.ru>
2025-07-15 15:31:51 +0700

    Update states.py

--- states.py
+++ states.py
@@ -1,7 +1,7 @@
-from aiogram.fsm.state import StatesGroup, State  
+from aiogram.fsm.state import StatesGroup, State
 
-class ProjectCreation(StatesGroup):  
-    name = State()  
-    brief = State()  
-    client = State()  
-    editors = State()  
+class ProjectCreation(StatesGroup):
+    name = State()
+    brief = State()
+    client = State()
+    editors = State()
